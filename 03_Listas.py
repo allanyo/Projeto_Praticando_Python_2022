34240daa5c41bb42357703901eea702490c3a846
allanyo <allanyosantos@gmail.com>
2022-10-18 21:27:25 -0300

    fiz um alteração, so de teste

--- 03_Listas.py
+++ 03_Listas.py
@@ -4,11 +4,13 @@ lista = ["Allanyo",26,1.84] #As lista em python suportam varios tipos de arquivo
 
 print(lista)
 
-lista.append(1995)
+lista.append(1995)# add dado a lista
 
 print(lista)
 
 len(lista)
-lista.append('Aparecida')
+lista.append('Aparecida')# add dado a lista
 
-print(len(lista))
+print(len(lista)) #imprimindo o tamanho da lista
+
+print("\nfinalizando o codigo")
